117e8467067a028c2642771b1db49f7215cbd162
etcd <etcd>
2023-01-21 04:05:38 +0000

    Test svg

--- source/ReactLineChart.tsx
+++ source/ReactLineChart.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 
-const Chart = () => {
-  return <div>hello world</div>;
-};
-
-export const render = (target: HTMLElement) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Chart />
-    </React.StrictMode>,
-    target
+const Chart = ({ width, height }: { width: number; height: number }) => {
+  return (
+    <svg width={width} height={height}>
+      this is an SVG
+    </svg>
   );
 };
+
+export const render =
+  ({ width, height }: { width: number; height: number }) =>
+  (target: HTMLElement) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <Chart width={width} height={height} />
+      </React.StrictMode>,
+      target
+    );
+  };
